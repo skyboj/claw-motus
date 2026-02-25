13228dce398ccbe2a264dca1bdaee3a4417b2ed1
skyboj <skyboj@gmail.com>
2026-02-24 16:20:08 -0800

    fix(hosting): resolve firebase deploy CI pathing and static sitemap exclusion

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     root: 'public',
     build: {
-        outDir: '../dist',
+        outDir: path.resolve(__dirname, 'dist'),
         emptyOutDir: true
     }
 });
